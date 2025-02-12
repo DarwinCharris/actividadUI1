0518810d432581f1b578795e3d8a097ba5f0f391
Darwin Charris <hdarwin@uninorte.edu.co>
2025-02-12 11:08:11 -0500

    Modules

--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -12,7 +12,9 @@ class FlutterCoursePage extends StatelessWidget {
         children: <Widget>[
           buildHeader(),
           buildCourseFocus(),
+          Divider(),
           buildModules(),
+          Divider(),
           buildProjectList(),
         ],
       ),
@@ -28,7 +30,98 @@ class FlutterCoursePage extends StatelessWidget {
   }
 
   Widget buildModules() {
-    return const Text("Modules");
+    return Column(
+      children: <Widget>[
+        const Text("Modules"),
+        Row(
+          children: <Widget>[
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 203, 203),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.book,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      const Text(
+                        "Introduction",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 203, 203),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.design_services,
+                            color: const Color.fromARGB(255, 109, 12, 12),
+                          )),
+                      const Text("UX desing", style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 203, 203),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.storage,
+                            color: const Color.fromARGB(255, 219, 126, 4),
+                          )),
+                      const Text("State management",
+                          style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 203, 203),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.bug_report,
+                            color: const Color.fromARGB(255, 40, 41, 40),
+                          )),
+                      const Text("Testing", style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                          width: 70,
+                          height: 90,
+                          decoration: BoxDecoration(
+                              color: const Color.fromARGB(255, 204, 203, 203),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Icon(
+                            Icons.network_check,
+                            color: const Color.fromARGB(255, 13, 24, 179),
+                          )),
+                      const Text("Networking", style: TextStyle(fontSize: 15)),
+                    ],
+                  ),
+                ]))
+          ],
+        ),
+      ],
+    );
   }
 
   Widget buildProjectList() {
