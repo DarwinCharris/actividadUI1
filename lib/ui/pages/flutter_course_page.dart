226e46e12bb2cf4a4b1415624d4324483f2cd430
Darwin Charris <hdarwin@uninorte.edu.co>
2025-02-12 11:23:13 -0500

    all

--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -22,11 +22,144 @@ class FlutterCoursePage extends StatelessWidget {
   }
 
   Widget buildHeader() {
-    return const Text("Flutter course");
+    return Column(
+      children: [
+        Container(
+          color: const Color.fromARGB(255, 37, 40, 195),
+          height: 150,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Flutter course',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  const Text(
+                    '2015-10 ',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    margin: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color.fromARGB(255, 14, 12, 12),
+                    ),
+                    child: Icon(
+                      Icons.search,
+                      color: const Color.fromARGB(1000, 254, 254, 254),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(5.0),
+                    margin: EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.0),
+                      color: const Color.fromARGB(255, 14, 12, 12),
+                    ),
+                    child: Icon(
+                      Icons.notifications,
+                      color: const Color.fromARGB(1000, 254, 254, 254),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   Widget buildCourseFocus() {
-    return const Text("Focus on the course");
+    return Column(
+      children: [
+        const Text(
+          'Course focus',
+          textAlign: TextAlign.left,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color.fromARGB(255, 0, 0, 0),
+            fontSize: 20.0,
+          ),
+        ),
+        Row(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(
+                    color: Color.fromARGB(199, 51, 9, 140),
+                    height: 5,
+                  ),
+                  const Text(
+                    'UI Development',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(color: Color.fromARGB(199, 245, 10, 10), height: 5),
+                  const Text(
+                    'Architecture',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Container(
+                      color: Color.fromARGB(199, 228, 251, 19), height: 5),
+                  const Text(
+                    'Desing Testing Thinking',
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 10.0,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ],
+    );
   }
 
   Widget buildModules() {
@@ -125,6 +258,120 @@ class FlutterCoursePage extends StatelessWidget {
   }
 
   Widget buildProjectList() {
-    return const Text("Projects");
+    return Column(
+      children: <Widget>[
+        const Text("Modules"),
+        Column(
+          children: <Widget>[
+            SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.folder,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                      Text("Introduction"),
+                      Container(
+                          color: Colors.grey,
+                          child: Icon(
+                            Icons.more_vert_rounded,
+                            color: const Color.fromARGB(255, 12, 65, 109),
+                          )),
+                    ],
+                  ),
+                ]))
+          ],
+        ),
+      ],
+    );
   }
 }
